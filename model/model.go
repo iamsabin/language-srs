f1f5783e7164c2bfc8c965581c703aba3b53f4ba
Sabin <mhr_sabin@hotmail.com>
2024-10-14 21:56:36 +1100

    Create anki model

--- model/model.go
+++ model/model.go
@@ -5,3 +5,13 @@ type Transliterate struct {
 	Kana     string
 	Meanings []string
 }
+
+type AnkiFormat struct {
+	Image              string `csv:"image"`
+	ReadingText        string `csv:"readingText"`
+	Audio              string `csv:"audio"`
+	AnswerText         string `csv:"answerText"`
+	AnswerTextFurigana string `csv:"answerTextFurigana"`
+	SortOrder          string `csv:"sortOrder"`
+	OriginalText       string `csv:"originalText"`
+}
