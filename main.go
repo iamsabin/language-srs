df469c29dad94a9e7c13213b444e19102a4dd345
Sabin <mhr_sabin@hotmail.com>
2024-10-20 20:45:25 +1100

    Create main

--- main.go
+++ main.go
@@ -1,72 +1,9 @@
 package main
 
-import (
-	"os"
-
-	"language-srs/anki"
-	"language-srs/known"
-
-	"github.com/jszwec/csvutil"
-
-	"language-srs/model"
-	"language-srs/transliterate"
-	"language-srs/wanikani"
-)
-
-type Input struct {
-	Japanese string `csv:"Japanese"`
-	English  string `csv:"English"`
-}
+import "language-srs/service"
 
 func main() {
-	inputFile := "goodbye-aitomioka"
+	srv := service.NewService()
 
-	input := getInput(inputFile)
-
-	var transliterated []model.Transliterate
-	for _, i := range input {
-		o := transliterate.Transliterate(i.Japanese)
-		transliterated = append(transliterated, o...)
-		// panic("done")
-	}
-
-	knownSubjects := wanikani.GetSubjects()
-	knownSubjectsFromMemory := known.GetSubjects()
-	knownSubjects = append(knownSubjects, knownSubjectsFromMemory...)
-
-	var unknownTransliterated []model.Transliterate
-
-	for _, t := range transliterated {
-		if !hasInKnown(t, knownSubjects) {
-			unknownTransliterated = append(unknownTransliterated, t)
-		}
-	}
-
-	// tangochou.CreateSRSDeck(unknownTransliterated, inputFile)
-	anki.CreateSRSDeck(unknownTransliterated, inputFile)
-}
-
-func hasInKnown(t model.Transliterate, knownSubjects []wanikani.Subject) bool {
-	for _, subject := range knownSubjects {
-		if subject.Text == t.Kanji || subject.Text == t.Kana {
-			return true
-		}
-	}
-
-	return false
-}
-
-func getInput(inputFile string) []Input {
-	var input []Input
-
-	jpen, err := os.ReadFile("input/" + inputFile + ".csv")
-	if err != nil {
-		panic(err)
-	}
-	err = csvutil.Unmarshal(jpen, &input)
-	if err != nil {
-		panic(err)
-	}
-
-	return input
+	srv.CreateEnglishToJapaneseDeck()
 }
--- service/service.go
+++ service/service.go
@@ -1,13 +1,23 @@
 package service
 
 import (
+	"os"
+
+	"github.com/jszwec/csvutil"
+
 	"language-srs/model"
 	"language-srs/repository"
+	"language-srs/repository/wanikani"
+	"language-srs/transliterate"
 )
 
 type Service interface {
 	CreateJapaneseToEnglishDeck()
 	CreateEnglishToJapaneseDeck()
+}
+
+func NewService() Service {
+	return service{}
 }
 
 type service struct {
@@ -17,8 +27,61 @@ type service struct {
 }
 
 func (s service) CreateJapaneseToEnglishDeck() {
-	// TODO implement me
-	panic("implement me")
+	inputFile := "goodbye-aitomioka"
+
+	input := getInput(inputFile)
+
+	var transliterated []model.Transliterate
+	for _, i := range input {
+		o := transliterate.Transliterate(i.Japanese)
+		transliterated = append(transliterated, o...)
+		// panic("done")
+	}
+
+	// knownSubjects := wanikani.GetSubjects()
+	// knownSubjectsFromMemory := known.GetSubjects()
+	// knownSubjects = append(knownSubjects, knownSubjectsFromMemory...)
+	//
+	// var unknownTransliterated []model.Transliterate
+	//
+	// for _, t := range transliterated {
+	// 	if !hasInKnown(t, knownSubjects) {
+	// 		unknownTransliterated = append(unknownTransliterated, t)
+	// 	}
+	// }
+	//
+	// // tangochou.CreateSRSDeck(unknownTransliterated, inputFile)
+	// anki.CreateSRSDeck(unknownTransliterated, inputFile)
+}
+
+func hasInKnown(t model.Transliterate, knownSubjects []wanikani.Subject) bool {
+	for _, subject := range knownSubjects {
+		if subject.Text == t.Kanji || subject.Text == t.Kana {
+			return true
+		}
+	}
+
+	return false
+}
+
+type Input struct {
+	Japanese string `csv:"Japanese"`
+	English  string `csv:"English"`
+}
+
+func getInput(inputFile string) []Input {
+	var input []Input
+
+	jpen, err := os.ReadFile("input/" + inputFile + ".csv")
+	if err != nil {
+		panic(err)
+	}
+	err = csvutil.Unmarshal(jpen, &input)
+	if err != nil {
+		panic(err)
+	}
+
+	return input
 }
 
 func (s service) CreateEnglishToJapaneseDeck() {
@@ -33,8 +96,4 @@ func (s service) CreateEnglishToJapaneseDeck() {
 
 	s.ankiRepo.CreateImmersionDecks(allImmersionAnki,
 		"recentmistakes-context-sentences-deck.csv")
-}
-
-func NewService() Service {
-	return service{}
 }
