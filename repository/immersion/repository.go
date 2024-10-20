4039f6776882e1c26b89957c2903b88743cb60c4
Sabin <mhr_sabin@hotmail.com>
2024-10-20 21:09:13 +1100

    Format immersion repository

--- repository/immersion/repository.go
+++ repository/immersion/repository.go
@@ -3,7 +3,8 @@ package immersion
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
+	"log/slog"
 	"net/http"
 
 	"language-srs/model"
@@ -11,40 +12,60 @@ import (
 )
 
 type repo struct {
+	maxNumber int
 }
 
 func NewRepository() repository.ImmersionRepository {
-	return &repo{}
+	return &repo{
+		maxNumber: 3,
+	}
 }
 
-func (r repo) GetImmersionInfo(keyword model.WaniKaniSubject) ([]model.
-ImmersionAnkiFormat, error) {
+func (r repo) GetImmersionInfo(keyword model.WaniKaniSubject) (
+	[]model.
+		ImmersionAnkiFormat, error) {
 	// TODO: if count/output for drama is zero, use anime
 	// Define the API endpoint with the query parameters
-	apiURL := fmt.Sprintf("https://api.immersionkit.com/look_up_dictionary?keyword=%s&category=drama&sort=shortness&wk=18",
+	apiURL := fmt.Sprintf(
+		"https://api.immersionkit.com/look_up_dictionary?keyword=%s&category=drama&sort=shortness&wk=18",
 		keyword.Text)
 
 	// Make the HTTP GET request
 	resp, err := http.Get(apiURL)
 	if err != nil {
-		fmt.Printf("Failed to make request: %v\n", err)
-		return nil, err
+		requestErr := fmt.Errorf(
+			"failed to make request for url: %s, err: %w", apiURL, err)
+		slog.Error(requestErr.Error())
+		return nil, requestErr
 	}
-	defer resp.Body.Close()
+	defer func(Body io.ReadCloser) {
+		bodyCloseErr := Body.Close()
+		if bodyCloseErr != nil {
+			bodyCloseError := fmt.Errorf(
+				"failed to close body for url: %s,"+
+					" err: %w", apiURL, bodyCloseErr)
+			slog.Error(bodyCloseError.Error())
+		}
+	}(resp.Body)
 
 	// Read the response body
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Printf("Failed to read response body: %v\n", err)
-		return nil, err
+		requestErr := fmt.Errorf(
+			"failed to read response body for url: %s, err: %w", apiURL, err)
+		slog.Error(requestErr.Error())
+		return nil, requestErr
 	}
 
 	// Parse the JSON response
 	var response Response
 	err = json.Unmarshal(body, &response)
 	if err != nil {
-		fmt.Printf("Failed to parse JSON response: %v\n", err)
-		return nil, err
+		unmarshallErr := fmt.Errorf(
+			"failed to parse JSON response for url: %s, "+
+				"err: %w", apiURL, err)
+		slog.Error(unmarshallErr.Error())
+		return nil, unmarshallErr
 	}
 
 	var ankiFormats []model.ImmersionAnkiFormat
@@ -52,18 +73,19 @@ ImmersionAnkiFormat, error) {
 	for _, item := range response.Data {
 
 		for i, v := range item.Examples {
-			if i > 2 {
+			if i > r.maxNumber-1 {
 				break
 			}
-			ankiFormats = append(ankiFormats, model.ImmersionAnkiFormat{
-				Image:              v.ImageUrl,
-				ReadingText:        v.Sentence,
-				Audio:              v.SoundUrl,
-				AnswerTextFurigana: v.SentenceWithFurigana,
-				AnswerText:         v.Translation,
-				SortOrder:          keyword.ID,
-				OriginalText:       keyword.Text,
-			})
+			ankiFormats = append(
+				ankiFormats, model.ImmersionAnkiFormat{
+					Image:              v.ImageUrl,
+					ReadingText:        v.Sentence,
+					Audio:              v.SoundUrl,
+					AnswerTextFurigana: v.SentenceWithFurigana,
+					AnswerText:         v.Translation,
+					SortOrder:          keyword.ID,
+					OriginalText:       keyword.Text,
+				})
 		}
 	}
 
